9c0c6d7d31a88017f56f2a6f6a2c0264c988563e
andrew <stockto2@email.arizona.edu>
2016-09-05 15:31:52 -0700

    Added basic three.js demo with human figure.

--- htdocs/js/3ddemo.js
+++ htdocs/js/3ddemo.js
@@ -1,124 +1,218 @@
 $(document).ready(function(){
 
-    var SCREEN_WIDTH = window.innerWidth;
-    var SCREEN_HEIGHT = window.innerHeight;
-    var container;
-    var camera, scene;
-    var renderer;
-    var mesh;
-    var mouseX = 0, mouseY = 0;
-    var windowHalfX = window.innerWidth / 2;
-    var windowHalfY = window.innerHeight / 2;
-    var clock = new THREE.Clock();
-    var gui, skinConfig, morphConfig;
-    
-    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+		/*global variables*/
+		var scene, camera, renderer;
+		var controls, guiControls, datGUI;
+		var stats;
+		var spotLight, hemi;
+		var SCREEN_WIDTH, SCREEN_HEIGHT;
+		var loader, model;
+		
+
+		function init(){
+			/*creates empty scene object and renderer*/
+			scene = new THREE.Scene();
+			camera =  new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, .1, 500);
+			renderer = new THREE.WebGLRenderer({antialias:true});
+			
+			renderer.setClearColor(0x333300);
+			renderer.setSize(window.innerWidth, window.innerHeight);
+			renderer.shadowMapEnabled= true;
+			renderer.shadowMapSoft = true;
+			
+			/*add controls*/
+			controls = new THREE.OrbitControls( camera, renderer.domElement );
+			controls.addEventListener( 'change', render );
+						
+			camera.position.x = 200;
+			camera.position.y = 200;
+			camera.position.z = 200;    
+			camera.lookAt(scene.position);
+
+			/*datGUI controls object*/
+			guiControls = new function(){
+				this.Bone_0 = 0.0;
+				this.Bone_1 = 0.0;
+				this.Bone_2 = 0.0;
+				this.Bone_3 = 0.0;
+				
+				this.rotationX  = 0.0;
+				this.rotationY  = 0.0;
+				this.rotationZ  = 0.0;
+				
+				this.lightX = 131;
+				this.lightY = 107;
+				this.lightZ = 180;
+				this.intensity = 1.5;       
+				this.distance = 373;
+				this.angle = 1.6;
+				this.exponent = 38;
+				this.shadowCameraNear = 34;
+				this.shadowCameraFar = 2635;
+				this.shadowCameraFov = 68;
+				this.shadowCameraVisible=false;
+				this.shadowMapWidth=512;
+				this.shadowMapHeight=512;
+				this.shadowBias=0.00;
+				this.shadowDarkness=0.11;
+				
+				this.scene = function(){
+					console.log(scene);
+				};
+						   
+			}
+			
+			//add some nice lighting
+			hemi = new THREE.HemisphereLight( 0xff0090, 0xff0011 );
+			scene.add(hemi);
+			//add some fog
+			scene.fog = new THREE.Fog( 0xffff90, .01, 500 );
+	  
+			/*adds spot light with starting parameters*/
+			spotLight = new THREE.SpotLight(0xffffff);
+			spotLight.castShadow = true;
+			spotLight.position.set (20, 35, 40);
+			spotLight.intensity = guiControls.intensity;        
+			spotLight.distance = guiControls.distance;
+			spotLight.angle = guiControls.angle;
+			spotLight.exponent = guiControls.exponent;
+			spotLight.shadowCameraNear = guiControls.shadowCameraNear;
+			spotLight.shadowCameraFar = guiControls.shadowCameraFar;
+			spotLight.shadowCameraFov = guiControls.shadowCameraFov;
+			spotLight.shadowCameraVisible = guiControls.shadowCameraVisible;
+			spotLight.shadowBias = guiControls.shadowBias;
+			spotLight.shadowDarkness = guiControls.shadowDarkness;
+			scene.add(spotLight);
+			
+			/*add loader call add model function*/
+			loader = new THREE.JSONLoader();
+			loader.load( '/test/models/human.js', addModel );
+			
+			/*adds controls to scene*/
+			datGUI = new dat.GUI();
+			
+			/*edit bones*/
+			datGUI.add(guiControls, "scene");
+			var cfolder = datGUI.addFolder('Controls');
+			
+			cfolder.add(guiControls, 'Bone_0',-3.14, 3.14);     
+			cfolder.add(guiControls, 'Bone_1',-3.14, 3.14);
+			cfolder.add(guiControls, 'Bone_2',-3.14, 3.14);      
+			cfolder.add(guiControls, 'Bone_3',-3.14, 3.14);         
+			
+
+			
+			var lfolder = datGUI.addFolder('Lights');
+			lfolder.add(guiControls, 'lightX',-60,400); 
+			lfolder.add(guiControls, 'lightY',0,400);   
+			lfolder.add(guiControls, 'lightZ',-60,400);
+			
+			lfolder.add(guiControls, 'intensity',0.01, 5).onChange(function(value){
+				spotLight.intensity = value;
+			});     
+			lfolder.add(guiControls, 'distance',0, 1000).onChange(function(value){
+				spotLight.distance = value;
+			}); 
+			lfolder.add(guiControls, 'angle',0.001, 1.570).onChange(function(value){
+				spotLight.angle = value;
+			});     
+			lfolder.add(guiControls, 'exponent',0 ,50 ).onChange(function(value){
+				spotLight.exponent = value;
+			});
+			lfolder.add(guiControls, 'shadowCameraNear',0,100).name("Near").onChange(function(value){       
+				spotLight.shadowCamera.near = value;
+				spotLight.shadowCamera.updateProjectionMatrix();        
+			});
+			lfolder.add(guiControls, 'shadowCameraFar',0,5000).name("Far").onChange(function(value){
+				spotLight.shadowCamera.far = value;
+				spotLight.shadowCamera.updateProjectionMatrix();
+			});
+			lfolder.add(guiControls, 'shadowCameraFov',1,180).name("Fov").onChange(function(value){
+				spotLight.shadowCamera.fov = value;
+				spotLight.shadowCamera.updateProjectionMatrix();
+			});
+			lfolder.add(guiControls, 'shadowCameraVisible').onChange(function(value){
+				spotLight.shadowCameraVisible = value;
+				spotLight.shadowCamera.updateProjectionMatrix();
+			});
+			lfolder.add(guiControls, 'shadowBias',0,1).onChange(function(value){
+				spotLight.shadowBias = value;
+				spotLight.shadowCamera.updateProjectionMatrix();
+			});
+			lfolder.add(guiControls, 'shadowDarkness',0,1).onChange(function(value){
+				spotLight.shadowDarkness = value;
+				spotLight.shadowCamera.updateProjectionMatrix();
+			});
+			datGUI.close();
+			$("#editor").append(renderer.domElement);
+			/*stats*/
+			stats = new Stats();        
+			stats.domElement.style.position = 'absolute';
+			stats.domElement.style.left = '0px';
+			stats.domElement.style.top = '0px';     
+			$("#editor").append( stats.domElement );
+		}
+		var set = [];
+		var helpset = [];
+		var scaleVal = 3;
+		function addModel( geometry,  materials ){
+	  
+			for (var i = 0;i < 1; i++){
+				materials[0].skinning = true;
+
+				var cs = 15;
+				
+				set[i]= new THREE.SkinnedMesh( geometry, new THREE.MeshFaceMaterial(materials) );
+				set[i].position.set(0,0,0);
+				set[i].scale.set (cs, cs, cs);
+				set[i].castShadow = true;
+				set[i].receiveShadow = true;
+				
+				scene.add(set[i]);
+				helpset[i] = new THREE.SkeletonHelper(set[i]);
+				//scene.add(helpset[i]);
+			   
+			}        
+
+		}
+			
+		function render() { 
+			spotLight.position.x = guiControls.lightX;
+			spotLight.position.y = guiControls.lightY;
+			spotLight.position.z = guiControls.lightZ;
+			
+			scene.traverse(function(child){
+				if (child instanceof THREE.SkinnedMesh){  
+					
+					child.rotation.y += .01;
+					
+					child.skeleton.bones[0].rotation.z = guiControls.Bone_0;
+					child.skeleton.bones[1].rotation.z = guiControls.Bone_1;
+					child.skeleton.bones[2].rotation.z = guiControls.Bone_2;
+					child.skeleton.bones[3].rotation.z = guiControls.Bone_3;                
+				}
+				else if  (child instanceof THREE.SkeletonHelper){
+					child.update();
+				}
+			}); 
+
+		}
+		
+		function animate(){
+			requestAnimationFrame(animate);
+        render();
+        stats.update();     
+        renderer.render(scene, camera);
+    }
     
     init();
     animate();
-
-    function init() {
-        container = document.getElementById( 'editor' );
-        camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
-        camera.position.set( 2000, 5000, 5000 );
-        scene = new THREE.Scene();
-        // LIGHTS
-        var light = new THREE.DirectionalLight( 0xffffff, 1 );
-        light.position.set( 0, 140, 500 );
-        light.position.multiplyScalar( 1.1 );
-        light.color.setHSL( 0.6, 0.075, 1 );
-        scene.add( light );
-        //
-        var light = new THREE.DirectionalLight( 0xffffff, 1 );
-        light.position.set( 0, - 1, 0 );
-        scene.add( light );
-        // RENDERER
-        renderer = new THREE.WebGLRenderer( { antialias: true } );
-        renderer.setClearColor( 0x000000 );
-        renderer.setPixelRatio( window.devicePixelRatio );
-        renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
-        container.appendChild( renderer.domElement );
-        // CHARACTER
-        character = new THREE.UCSCharacter();
-        character.onLoadComplete = function () {
-            console.log( "Load Complete" );
-            console.log( character.numSkins + " skins and " + character.numMorphs + " morphtargets loaded." );
-            gui = new dat.GUI();
-            setupSkinsGUI();
-            setupMorphsGUI();
-            gui.width = 200;
-            
-            gui.open();
-        };
-        var loader = new THREE.XHRLoader();
-        loader.load( '/vendor/threejs/examples/models/skinned/UCS_config.json', function ( text ) {
-            var config = JSON.parse( text );
-            character.loadParts( config );
-            scene.add( character.root );
-        } );
-        window.addEventListener( 'resize', onWindowResize, false );
-        controls = new THREE.OrbitControls( camera, renderer.domElement );
-        controls.target.set( 0, 3000, 0 );
-        controls.addEventListener( 'change', render );
-    }
-    function setupSkinsGUI() {
-        var skinGui = gui.addFolder( "Skins" );
-        skinConfig = {
-            wireframe: false
-        };
-        var skinCallback = function ( index ) {
-            return function () {
-                character.setSkin( index );
-            };
-        };
-        for ( var i = 0; i < character.numSkins; i ++ ) {
-            var name = character.skins[ i ].name;
-            skinConfig[ name ] = skinCallback( i );
-        }
-        for ( var i = 0; i < character.numSkins; i ++ ) {
-            skinGui.add( skinConfig, character.skins[ i ].name );
-        }
-        skinGui.open();
-    }
-    function setupMorphsGUI() {
-        var morphGui = gui.addFolder( "Morphs" );
-        morphConfig = {
-        };
-        var morphCallback = function( index ) {
-            return function () {
-                character.updateMorphs( morphConfig );
-            }
-        };
-        for ( var i = 0; i < character.numMorphs; i ++ ) {
-            var morphName = character.morphs[ i ];
-            morphConfig[ morphName ] = 0;
-        }
-        for ( var i = 0; i < character.numMorphs; i ++ ) {
-            morphGui.add( morphConfig, character.morphs[ i ] ).min( 0 ).max( 100 ).onChange( morphCallback( i ) );
-        }
-        morphGui.open();
-    }
-    function onWindowResize() {
-        windowHalfX = window.innerWidth / 2;
-        windowHalfY = window.innerHeight / 2;
-        camera.aspect = window.innerWidth / window.innerHeight;
+    
+    $(window).resize(function(){
+        SCREEN_WIDTH = window.innerWidth;
+        SCREEN_HEIGHT = window.innerHeight;
+        camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
         camera.updateProjectionMatrix();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-    }
-    function onDocumentMouseMove( event ) {
-        mouseX = ( event.clientX - windowHalfX ) * 10;
-        mouseY = ( event.clientY - windowHalfY ) * 10;
-    }
-    //
-    function animate() {
-        requestAnimationFrame( animate );
-        controls.update();
-        render();
-    }
-    function render() {
-        var delta = 0.75 * clock.getDelta();
-        // update skinning
-        character.mixer.update( delta );
-        renderer.render( scene, camera );
-    }
+        renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
+    });	
 });
--- lib/views/editor.php
+++ lib/views/editor.php
@@ -1,19 +1,19 @@
 <header class="bg-inverse text-center center-vertically" role="banner">
   <div class="editor-container">
     <div id='threejs-demo'>
-
-	</div>
 	<div id='editor'> </div>
   </div>
+  
 </header>
 
 <!--Feature Specific Scripts (be sure they load after the js in the footer with document.ready-->
 <script src="/vendor/threejs/build/three.js"></script>
-<script src="/vendor/threejs/examples/js/UCSCharacter.js"></script>
-<script src="/vendor/threejs/examples/js/Detector.js"></script>
-<script src="/vendor/threejs/examples/js/libs/dat.gui.min.js"></script>
-<script src="/vendor/threejs/examples/js/controls/OrbitControls.js"></script>
+<script src="/vendor/threejs/external/OrbitControls.js"></script>
+<script src='/vendor/threejs/external/dat.gui.min.js'></script>
+<script src="/vendor/threejs/external/stats.min.js"></script>
+<script src="/vendor/threejs/external/STLExporter.js"></script>
 <script src="/js/3ddemo.js"></script>
+
 
 <style type="text/css">
 	body {
